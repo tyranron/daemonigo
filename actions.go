e97bc76591517192f17022dcaff68c39a71ce5bd
tyranron <tyranron@gmail.com>
2014-04-16 23:43:33 +0300

    add initial files

---
+++ actions.go
@@ -0,0 +1,135 @@
+package daemonigo
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var actMux *sync.Mutex = &sync.Mutex{}
+var actions map[string]func() = map[string]func(){
+	"start": func() {
+		switch isRunning, _, err := Status(); {
+		case err != nil:
+			printStatusErr(err)
+		case isRunning:
+			fmt.Println(Name + " is already started and running now")
+		default:
+			Start()
+		}
+	},
+	"stop": func() {
+		switch isRunning, process, err := Status(); {
+		case err != nil:
+			printStatusErr(err)
+		case !isRunning:
+			fmt.Println(Name + " is NOT running or already stopped")
+		default:
+			Stop(process)
+
+		}
+	},
+	"status": func() {
+		switch isRunning, process, err := Status(); {
+		case err != nil:
+			printStatusErr(err)
+		case !isRunning:
+			fmt.Println(Name + " is NOT running")
+		default:
+			fmt.Printf(Name+" is running with PID %d\n", process.Pid)
+		}
+	},
+	"restart": func() {
+		isRunning, process, err := Status()
+		if err != nil {
+			printStatusErr(err)
+			return
+		}
+		if isRunning {
+			Stop(process)
+		}
+		Start()
+	},
+}
+
+func printStatusErr(e error) {
+	fmt.Println("Checking status of " + Name + " failed")
+	fmt.Println("Details:", e.Error())
+}
+
+func Stop(process *os.Process) {
+	fmt.Print("Stopping " + Name + "...")
+	if err := process.Signal(os.Interrupt); err != nil {
+		failed(err)
+		return
+	}
+	for {
+		time.Sleep(200 * time.Millisecond)
+		switch isRunning, _, err := Status(); {
+		case err != nil:
+			printStatusErr(err)
+			return
+		case !isRunning:
+			fmt.Println("OK")
+			return
+		}
+	}
+}
+
+func Start() {
+	fmt.Print("Starting " + Name + "...")
+	path, err := filepath.Abs(AppPath)
+	if err != nil {
+		failed(err)
+		return
+	}
+	cmd := PrepareCommand(path)
+	if err = cmd.Start(); err != nil {
+		failed(err)
+		return
+	}
+	ch := make(chan bool)
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("OK")
+		ch <- true
+	}()
+	go func() {
+		err = cmd.Wait()
+		if err == nil {
+			err = fmt.Errorf("daemon stopped and not running")
+		}
+		failed(err)
+		ch <- true
+	}()
+	<-ch
+}
+
+func failed(e error) {
+	fmt.Println("FAILED")
+	fmt.Println("Details:", e.Error())
+}
+
+func PrepareCommand(path string) (cmd *exec.Cmd) {
+	cmd = exec.Command(path)
+	cmd.Env = append(
+		os.Environ(),
+		fmt.Sprintf("%s=%s", envVarName, envVarValue),
+	)
+	return
+}
+
+func HandleFunc(pattern string, handler func()) {
+	actMux.Lock()
+	defer actMux.Unlock()
+	if pattern == "" {
+		panic("daemonic: pattern cannot be empty")
+	}
+	if handler == nil {
+		panic("daemonic: nil handler")
+	}
+	actions[pattern] = handler
+}
---
+++ daemoni.go
@@ -0,0 +1,147 @@
+package daemonigo
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// Name of enviroment variable used to distinguish parent and child processes.
+const EnvVarName = "_DAEMONIGO"
+
+const (
+	envVarName  = "_GO_DAEMON"
+	envVarValue = "1"
+
+	fileMask = 0644
+	umask    = 027
+)
+
+var (
+	Name    = "daemon"
+	PidFile = "daemon.pid"
+	AppPath = "./" + filepath.Base(os.Args[0])
+
+	pidFile *os.File
+)
+
+func Daemonize(workDir string) (isDeamon bool, err error) {
+	const errLoc = "daemonic.Daemonize()"
+	isDeamon = os.Getenv(envVarName) == envVarValue
+	if len(workDir) != 0 {
+		if err = os.Chdir(workDir); err != nil {
+			err = fmt.Errorf("%s: changing working directory failed, reason -> %s", err.Error())
+			return
+		}
+	}
+	if isDeamon {
+		syscall.Umask(int(umask))
+		if _, err = syscall.Setsid(); err != nil {
+			err = fmt.Errorf("%s: setsid failed, reason -> %s", err.Error())
+			return
+		}
+		if pidFile, err = lockPidFile(); err != nil {
+			err = fmt.Errorf("%s: locking PID file failed, reason -> %s: ", err.Error())
+		}
+	} else {
+		if !flag.Parsed() {
+			flag.Parse()
+		}
+		action, exist := actions[flag.Arg(0)]
+		if exist {
+			action()
+		} else {
+			arr := make([]string, 0, len(actions))
+			for k, _ := range actions {
+				arr = append(arr, k)
+			}
+			fmt.Println("Usage: " + os.Args[0] + " {" + strings.Join(arr, "|") + "}")
+		}
+	}
+	return
+}
+
+func lockPidFile() (pidFile *os.File, err error) {
+	var file *os.File
+	file, err = os.OpenFile(PidFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fileMask)
+	if err != nil {
+		return
+	}
+	defer func() {
+		// file must be open whole runtime to keep lock on itself
+		if err != nil {
+			file.Close()
+		}
+	}()
+
+	if err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
+		return
+	}
+	var fileLen int
+	fileLen, err = fmt.Fprint(file, os.Getpid())
+	if err != nil {
+		return
+	}
+	if err = file.Truncate(int64(fileLen)); err != nil {
+		return
+	}
+
+	return file, err
+}
+
+func UnlockPidFile() {
+	if pidFile != nil {
+		syscall.Flock(int(pidFile.Fd()), syscall.LOCK_UN)
+		pidFile.Close()
+	}
+}
+
+func Status() (isRunning bool, pr *os.Process, e error) {
+	var (
+		err  error
+		file *os.File
+	)
+
+	file, err = os.Open(PidFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			e = fmt.Errorf("could not open PID file: " + err.Error())
+		}
+		return
+	}
+	defer file.Close()
+	fd := int(file.Fd())
+	if err = syscall.Flock(fd, syscall.LOCK_EX|syscall.LOCK_NB); err != syscall.EWOULDBLOCK {
+		if err == nil {
+			syscall.Flock(fd, syscall.LOCK_UN)
+		} else {
+			e = fmt.Errorf("PID file locking attempt failed: " + err.Error())
+		}
+		return
+	}
+
+	isRunning = true
+	var n, pid int
+	content := make([]byte, 128)
+	n, err = file.Read(content)
+	if err != nil && err != io.EOF {
+		e = fmt.Errorf("could not read from PID file: " + err.Error())
+		return
+	}
+	pid, err = strconv.Atoi(string(content[:n]))
+	if n < 1 || err != nil {
+		e = fmt.Errorf("bad PID format, PID file is possibly corrupted")
+		return
+	}
+	pr, err = os.FindProcess(pid)
+	if err != nil {
+		e = fmt.Errorf("cannot find process by PID: " + err.Error())
+	}
+
+	return
+}
