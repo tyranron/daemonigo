d01227d27fb56723adfda64437d5e1027b40323e
tyranron <tyranron@gmail.com>
2014-04-20 10:23:41 +0300

    update echo server example with comments

--- examples/echo_server/main.go
+++ examples/echo_server/main.go
@@ -1,3 +1,4 @@
+// A simple echo server implementation from net.Listener official documentation.
 package main
 
 import (
@@ -8,26 +9,35 @@ import (
 )
 
 func main() {
+	// Daemonizing echo server application.
 	switch isDaemon, err := daemon.Daemonize(); {
 	case !isDaemon:
 		return
 	case err != nil:
-		log.Fatalf("Error: could not start daemon, reason -> %s", err.Error())
+		log.Fatalf("main(): could not start daemon, reason -> %s", err.Error())
 		return
 	}
+	// From now we are running in daemon process.
 
+	// Listen on TCP port 2000 on all interfaces.
 	l, err := net.Listen("tcp", ":2000")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 	for {
+		// Wait for a connection.
 		conn, err := l.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
 		go func(c net.Conn) {
+			// Echo all incoming data.
 			io.Copy(c, c)
+			// Shut down the connection.
 			c.Close()
 		}(conn)
 	}
