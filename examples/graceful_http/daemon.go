4da48ac64c0da4a813a99d8c884a8bd6245e61c7
tyranron <tyranron@gmail.com>
2014-04-22 10:57:36 +0300

    upd gareceful_http example with previousListener

--- examples/graceful_http/daemon.go
+++ examples/graceful_http/daemon.go
@@ -3,9 +3,10 @@ package main
 import (
 	daemon "github.com/tyranron/daemonigo"
 )
-
+/*
 func init() {
 	daemon.SetAction("reload", func() {
 
 	})
 }
+*/
--- examples/graceful_http/main.go
+++ examples/graceful_http/main.go
@@ -9,6 +9,8 @@ import (
 	"sync"
 )
 
+const envVarName = "_GO_FD"
+
 func main() {
 	// Daemonizing http server.
 	switch isDaemon, err := daemon.Daemonize(); {
@@ -36,6 +38,41 @@ func main() {
 	waiter.Wait()
 }
 
+// Helper function which checks environment variables
+// for socket descriptors and starts listening on it if any.
+func previousListener() (l net.Listener, hasPrev bool, e error) {
+	const errLoc = "main.Listener()"
+	var fd uintptr
+	if _, e = fmt.Sscan(os.Getenv(envVarName), &fd); e != nil {
+		e = fmt.Errorf("%s: could not read file descriptor from environment, reason -> %s", errLoc, e.Error())
+		return
+	}
+	hasPrev = true
+	if l, e = net.FileListener(os.NewFile(fd, "parent socket")); e != nil {
+		e = fmt.Errorf("%s: could not listen on old file descriptor, reason -> %s", errLoc, e.Error())
+		return
+	}
+	switch l.(type) {
+	case *net.TCPListener, *net.UnixListener:
+	default:
+		e = fmt.Errorf("%s: file descriptor is %T not *net.TCPListener or *net.UnixListener", errLoc, l)
+		return
+	}
+	if e = syscall.Close(int(fd)); e != nil {
+		e = fmt.Errorf("%s: failed to close file descriptor, reason -> %s", errLoc, e.Error())
+	}
+	return
+}
+
+// Checks if received error during serving
+// is appeared because of closing listener.
+func isErrClosing(err error) bool {
+	if opErr, ok := err.(*net.OpError); ok {
+		err = opErr.Err
+	}
+	return "use of closed network connection" == err.Error()
+}
+
 // A goroutine which listens OS signals.
 func listenSignals(l net.Listener) {
 	sigChan := make(chan os.Signal)
@@ -52,11 +89,7 @@ func listenSignals(l net.Listener) {
 	}
 }
 
-// Checks if received error during serving
-// is appeared because of closing listener.
-func isErrClosing(err error) bool {
-	if opErr, ok := err.(*net.OpError); ok {
-		err = opErr.Err
-	}
-	return "use of closed network connection" == err.Error()
+
+func reload(l net.Listener) {
+	l.Close()
 }
